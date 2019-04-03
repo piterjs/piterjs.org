2755bb977407bbbc98258f9a85b04953ddefcbc1
Mikhail Poluboyarinov <mikle.sol@gmail.com>
2019-04-03 16:34:15 +0300

    update build

--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
 wget -O ./meetup/meetup.data.json "https://api.airtable.com/v0/app23d1bSu43arYuu/Events?fields[]=Date%20from&fields[]=Event%20number&fields[]=Place&fields[]=Speakers&fields[]=Speeches&api_key=$AIRTABLE_KEY"
 wget -O ./speech/speech.data.json "https://api.airtable.com/v0/app23d1bSu43arYuu/Speeches?fields[]=Description&fields[]=Name&fields[]=Slides&fields[]=Video&fields[]=Speakers&fields[]=Topics&api_key=$AIRTABLE_KEY"
 wget -O ./speaker/speaker.data.json "https://api.airtable.com/v0/app23d1bSu43arYuu/Speakers?fields[]=Company%20%26%20position&fields[]=Name&fields[]=Notes&fields[]=Photo&api_key=$AIRTABLE_KEY"
-git commit -a -m "Data updated $( date )"
