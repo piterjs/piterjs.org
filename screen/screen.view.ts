2999401fc3747119782f6280b311bbc9ed7b711c
nin-jin <sairi-na-tenshi@ya.ru>
2020-02-17 17:36:56 +0300

    Don't recolor to place

--- screen/screen.view.ts
+++ screen/screen.view.ts
@@ -2,13 +2,13 @@ namespace $.$$ {
 
 	export class $piterjs_screen extends $.$piterjs_screen {
 
-		color_open() {
-			return this.place().colors()[0]
-		}
+// 		color_open() {
+// 			return this.place().colors()[0]
+// 		}
 
-		color_close() {
-			return this.place().colors()[1] || this.color_open()
-		}
+// 		color_close() {
+// 			return this.place().colors()[1] || this.color_open()
+// 		}
 
 		sub() {
 			return [
