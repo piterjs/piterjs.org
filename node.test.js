7c9d8d1aa82ac00c11dee3c959fba0c423b04027
nin-jin <nin-jin@users.noreply.github.com>
2023-03-01 19:19:42 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -5409,11 +5409,10 @@ var $;
         const { rem } = $mol_style_unit;
         $mol_style_define($piterjs_speech_snippet, {
             display: 'flex',
-            padding: rem(.25),
+            padding: $mol_gap.block,
             alignItems: 'flex-start',
             Photo: {
                 width: rem(6),
-                margin: $mol_gap.block,
             },
             Info: {
                 flex: {
@@ -5421,17 +5420,16 @@ var $;
                     shrink: 1,
                     basis: rem(16),
                 },
-                padding: rem(.75),
             },
             Title: {
                 display: 'block',
-                padding: [rem(.25), rem(.5)],
+                padding: $mol_gap.text,
             },
             Addon: {
                 display: 'flex',
             },
             Speaker_title: {
-                padding: [rem(.25), rem(.5)],
+                padding: $mol_gap.text,
                 color: $mol_theme.text,
                 flex: {
                     grow: 1,
@@ -5440,7 +5438,7 @@ var $;
                 },
             },
             Time: {
-                padding: [rem(.25), rem(.5)],
+                padding: $mol_gap.text,
                 color: $mol_theme.text,
                 whiteSpace: 'nowrap',
             },
--- web.js
+++ web.js
@@ -5368,11 +5368,10 @@ var $;
         const { rem } = $mol_style_unit;
         $mol_style_define($piterjs_speech_snippet, {
             display: 'flex',
-            padding: rem(.25),
+            padding: $mol_gap.block,
             alignItems: 'flex-start',
             Photo: {
                 width: rem(6),
-                margin: $mol_gap.block,
             },
             Info: {
                 flex: {
@@ -5380,17 +5379,16 @@ var $;
                     shrink: 1,
                     basis: rem(16),
                 },
-                padding: rem(.75),
             },
             Title: {
                 display: 'block',
-                padding: [rem(.25), rem(.5)],
+                padding: $mol_gap.text,
             },
             Addon: {
                 display: 'flex',
             },
             Speaker_title: {
-                padding: [rem(.25), rem(.5)],
+                padding: $mol_gap.text,
                 color: $mol_theme.text,
                 flex: {
                     grow: 1,
@@ -5399,7 +5397,7 @@ var $;
                 },
             },
             Time: {
-                padding: [rem(.25), rem(.5)],
+                padding: $mol_gap.text,
                 color: $mol_theme.text,
                 whiteSpace: 'nowrap',
             },
