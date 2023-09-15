c1730d2da629f02e8b6c3bb72fbcc0662b8f46e7
nin-jin <sairi-na-tenshi@ya.ru>
2023-09-15 13:36:32 +0300

    Update model.ts

--- model/model.ts
+++ model/model.ts
@@ -15,43 +15,6 @@ namespace $ {
 			return this.description_node().selection( this.land.peer().id, next )
 		}
 
-		// @ $mol_mem_key
-		// static item< Model extends typeof $piterjs_model >( this : Model , id : string ) {
-		// 	const model = new this as InstanceType< Model >
-		// 	model.id = $mol_const( $mol_data_string( id ) )
-		// 	return model
-		// }
-		
-		// @ $mol_mem
-		// static list< Model extends typeof $piterjs_model >( this : Model ) {
-		// 	return $mol_data_array(
-		// 		id => this.item( id ) as InstanceType< Model >
-		// 	)
-		// }
-
-		// @ $mol_mem
-		// static all< Model extends typeof $piterjs_model >( this : Model ) {
-		// 	return this.list()(
-		// 		Object.keys( this.data() )
-		// 	)
-		// }
-
-		// @ $mol_mem
-		// static data() {
-			
-		// 	const tree = $mol_tree.fromString( this.$.$mol_fetch.text( this.uri() ) )
-		// 	const json = new $mol_tree({ type : '*' , sub : tree.sub }).toJSON()
-
-		// 	return json
-
-		// }
-
-		// @ $mol_mem
-		// data( next? : any ) {
-		// 	const Model = this.constructor as typeof $piterjs_model
-		// 	return Model.data()[ this.id() ]
-		// }
-
 	}
 
 }
