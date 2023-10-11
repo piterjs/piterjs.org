32b2e44d65a6994fb430fc3cbd7a3d8a04b71411
nin-jin <sairi-na-tenshi@ya.ru>
2023-10-11 10:15:29 +0000

    visitors cheking

--- meetup/stats/stats.view.ts
+++ meetup/stats/stats.view.ts
@@ -4,8 +4,8 @@ namespace $.$$ {
 		@ $mol_mem
 		joins_stat() {
 			return $mol_array_groups(
-				Object.values( this.joined_moments() ),
-				moment => moment.toString( 'MM-DD' ),
+				Object.entries( this.joined_moments() ),
+				([ id, moment ])=> moment.toString( 'MM-DD' ),
 			)
 		}
 		
@@ -16,12 +16,22 @@ namespace $.$$ {
 		
 		@ $mol_mem
 		joins_per_days() {
-			return Object.values( this.joins_stat() ).map( moments => moments!.length )
+			return Object.values( this.joins_stat() ).map( pairs => pairs!.length )
 		}
 
 		@ $mol_mem
 		joins_title() {
 			return super.joins_title() + ` (${ Object.keys( this.joined_moments() ).length })`
+		}
+		
+		@ $mol_mem
+		visits_per_days() {
+			return Object.values( this.joins_stat() ).map( pairs => pairs!.filter( ([ id ])=> this.visitor( id ) ).length )
+		}
+
+		@ $mol_mem
+		visits_title() {
+			return super.visits_title() + ` (${ this.visitors_list().length })`
 		}
 		
 	}
