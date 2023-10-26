19882a42a78a86b163633857ea15fa270347cae7
nin-jin <nin-jin@users.noreply.github.com>
2023-10-26 10:55:23 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.mjs
+++ web.mjs
@@ -30772,7 +30772,7 @@ var $;
         }
         Descr() {
             const obj = new this.$.$mol_paragraph();
-            obj.title = () => "Ежемесячные митапы в Питере о JavaScript, Node.js и всём, что с ними связано";
+            obj.title = () => "Ежемесячные митапы в Питере о JavaScript, Node.js и всего, что с ними связано";
             return obj;
         }
         Join() {
