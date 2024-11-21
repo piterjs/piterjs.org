2192296aa615b56262794e0c097a6983ea94f2fd
nin-jin <nin-jin@users.noreply.github.com>
2024-11-21 15:04:01 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.mjs
+++ web.mjs
@@ -21851,7 +21851,7 @@ var $;
 		Sponsors(){
 			const obj = new this.$.$piterjs_intro_page();
 			(obj.title) = () => ("Спонсоры");
-			(obj.text) = () => ("\"\"https://habrastorage.org/webt/lj/hn/vy/ljhnvybz0o31ylka0kqpff8mnme.png\"\"\n\"\"https://habrastorage.org/webt/bh/lf/1i/bhlf1iphyupibm0-0lxbxm_srem.png\"\"\n\"\"https://habrastorage.org/webt/86/lx/yk/86lxykzdesrhsh9eqtezoizu02e.png\"\"");
+			(obj.text) = () => ("\"\"https://habrastorage.org/webt/lj/hn/vy/ljhnvybz0o31ylka0kqpff8mnme.png\"\"\n\"\"https://habrastorage.org/webt/b_/id/8y/b_id8y1art8zbhni8dgkzeegwfi.png\"\"\n\"\"https://habrastorage.org/webt/86/lx/yk/86lxykzdesrhsh9eqtezoizu02e.png\"\"");
 			return obj;
 		}
 		Contribution(){
