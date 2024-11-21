9368104690d5306cd5460a5934ab94b064cb6f3a
nin-jin <nin-jin@users.noreply.github.com>
2024-11-21 15:15:47 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -22364,7 +22364,7 @@ var $;
 		Sponsors(){
 			const obj = new this.$.$piterjs_intro_page();
 			(obj.title) = () => ("Спонсоры");
-			(obj.text) = () => ("\"\"https://habrastorage.org/webt/lj/hn/vy/ljhnvybz0o31ylka0kqpff8mnme.png\"\"\n\"\"https://habrastorage.org/webt/w4/g6/6c/w4g66cuggs-skuvz2vnpspdtm_g.png\"\"\n\"\"https://habrastorage.org/webt/b_/id/8y/b_id8y1art8zbhni8dgkzeegwfi.png\"\"\n\"\"https://habrastorage.org/webt/86/lx/yk/86lxykzdesrhsh9eqtezoizu02e.png\"\"\n\"\"https://habrastorage.org/webt/ox/mj/lk/oxmjlka1sl-tyukhmzre87ndlxs.png\"\"\"");
+			(obj.text) = () => ("\"\"https://habrastorage.org/webt/lj/hn/vy/ljhnvybz0o31ylka0kqpff8mnme.png\"\"\n\"\"https://habrastorage.org/webt/w4/g6/6c/w4g66cuggs-skuvz2vnpspdtm_g.png\"\"\n\"\"https://habrastorage.org/webt/b_/id/8y/b_id8y1art8zbhni8dgkzeegwfi.png\"\"\n\"\"https://habrastorage.org/webt/fn/ji/dj/fnjidjev1ht4q_dchp4d_jdipu8.png\"\"");
 			return obj;
 		}
 		Contribution(){
