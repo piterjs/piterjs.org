6142f819d0289eb2571b45bc38fe1ef2950b8f04
jin <jin@hyoo.ru>
2025-02-13 13:01:06 +0300

    + love

--- app/app.view.ts
+++ app/app.view.ts
@@ -43,6 +43,8 @@ namespace $.$$ {
 
 		safe() { return this.$.$mol_state_arg.value( 'safe' ) !== null }
 		
+		love() { return this.$.$mol_state_arg.value( 'love' ) !== null }
+		
 		@ $mol_mem
 		meetup_id( next? : string | null ) {
 			
@@ -84,6 +86,7 @@ namespace $.$$ {
 				... this.stats() ? [ this.Meetup_stats( this.meetup_id() ) ] : [] ,
 				... this.others() ? [ this.Others() ] : [] ,
 				... this.wiki() ? this.Wiki().pages() : [],
+				... this.love() ? this.Love().pages() : [],
 			]
 			if( pages.length === 1 ) pages.push( this.Now() )
 			return pages
