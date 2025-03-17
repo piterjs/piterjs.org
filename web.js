865dca40763ff04bb7f644af6ba3b566b7e26a42
nin-jin <sairi-na-tenshi@ya.ru>
2025-03-17 07:02:34 +0000

    Deploying to gh-pages from @ piterjs/piterjs.org@f91602f8e7f1f9ee39aa1dabb7c577aff2d8c78b 🚀

--- web.js
+++ web.js
@@ -30099,7 +30099,7 @@ var $;
 		Team(){
 			const obj = new this.$.$piterjs_intro_page();
 			(obj.title) = () => ("Команда");
-			(obj.text) = () => ("! \"\"https://i.imgur.com/KoX0d8g.jpeg\"\"\n  ! \"\"https://habrastorage.org/webt/mb/oz/2-/mboz2-qxenahgayijecyblbo6ka.jpeg\"\"\n    ! \"\"https://i.imgur.com/KiAK2i0.jpeg\"\"\n      ! \"\"https://i.imgur.com/V2Rkj38.jpeg\"\"\n! \"\"https://habrastorage.org/webt/xi/xd/hk/xixdhke23wtbhpnaz7gyuifpxnm.jpeg\"\"\n  ! \"\"https://i.imgur.com/n2Fxkqv.png\"\"\n    ! \"\"https://i.imgur.com/FS8bvSe.jpeg\"\"\n      ! \"\"https://i.imgur.com/R86XxcW.jpeg\"\"");
+			(obj.text) = () => ("! \"\"https://i.imgur.com/KoX0d8g.jpeg\"\"\n  ! \"\"https://habrastorage.org/webt/mb/oz/2-/mboz2-qxenahgayijecyblbo6ka.jpeg\"\"\n    ! \"\"https://i.imgur.com/KiAK2i0.jpeg\"\"\n      ! \"\"https://i.imgur.com/V2Rkj38.jpeg\"\"\n! \"\"https://i.imgur.com/qU059B2.jpeg\"\"\n  ! \"\"https://i.imgur.com/n2Fxkqv.png\"\"\n    ! \"\"https://i.imgur.com/FS8bvSe.jpeg\"\"\n      ! \"\"https://i.imgur.com/R86XxcW.jpeg\"\"");
 			return obj;
 		}
 		Speakers(){
