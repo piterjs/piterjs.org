9f24e06fdbbf2b417021f678db15f2936cf25e91
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-04-21 13:11:41 +0300

    фикс списка пользователей что бы не пропадал

--- meetup/guests/guests.view.ts
+++ meetup/guests/guests.view.ts
@@ -1,12 +1,27 @@
 namespace $.$$ {
 	export class $piterjs_meetup_guests extends $.$piterjs_meetup_guests {
-		
+
+		@ $mol_mem_key
+		person_name_safe( id: $mol_int62_string ) {
+			try {
+				return this.person( id )
+			} catch( error ) {
+				if( $mol_promise_like( error ) ) return id
+				throw error
+			}
+		}
+
+		@ $mol_mem
+		joined_list_unique() {
+			return [ ... new Set( this.meetup().joined_list() ) ]
+		}
+
 		@ $mol_mem
 		person_list() {
 			const moments = this.meetup().joined_moments()
-			return this.meetup().joined_list()
-				.filter( $mol_match_text( this.filter(), person => [ this.person( person ), person ] ) )
-				.sort( ( a, b )=> moments[ a ].valueOf() - moments[ b ].valueOf() )
+			return this.joined_list_unique()
+				.filter( $mol_match_text( this.filter(), person => [ this.person_name_safe( person ), person ] ) )
+				.sort( ( a, b )=> ( moments[ a ]?.valueOf() ?? 0 ) - ( moments[ b ]?.valueOf() ?? 0 ) )
 				.map( person => this.Person( person ) )
 		}
 
@@ -17,8 +32,8 @@ namespace $.$$ {
 
 		@ $mol_mem
 		dump_blob() {
-			const text = this.meetup().joined_list()
-				.map( person => this.person( person ) )
+			const text = this.joined_list_unique()
+				.map( person => this.person_name_safe( person ) )
 				.sort()
 				.join( '\n' )
 			return new $mol_blob( [ text ], { type: 'text/plain' } )
@@ -26,8 +41,9 @@ namespace $.$$ {
 
 		@ $mol_mem_key
 		person_join_moment( id: $mol_int62_string ) {
-			return this.meetup().joined_moments()[ id ].toString( `DD WD hh:mm` )
+			const moment = this.meetup().joined_moments()[ id ]
+			return moment ? moment.toString( `DD WD hh:mm` ) : ''
 		}
-		
+
 	}
 }
